8be3a5fad26ccf6205ad35023f737e575378a7e5
Denver Massey <denmas22@live.unc.edu>
2021-12-22 19:23:29 -0500

    fixing websocket connection issues'

--- multiplayer/public/cursors/main.js
+++ multiplayer/public/cursors/main.js
@@ -32,7 +32,7 @@ window.onload = async function() {
 }
 
 async function connectToServer() {
-    const ws = new WebSocket('ws://localhost:3000', 'cursors');
+    const ws = new WebSocket('ws://afternoon-plateau-82522.herokuapp.com/:3000', 'cursors');
     return new Promise((resolve, reject) => {
         const timer = setInterval(() => {
             if(ws.readyState === 1) {
