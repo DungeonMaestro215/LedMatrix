91e892f43870ce24d3cf5a744056ec96f4ce0c0d
Denver Massey <denmas22@live.unc.edu>
2022-02-08 18:32:23 -0500

    cleaning up some code

--- multiplayer/app.js
+++ multiplayer/app.js
@@ -20,12 +20,8 @@ app.use(favicon(fav_path));
 const port = process.env.PORT || 3000;
 
 // Initialize WebSockets
-// const wsport = 3000;
 const ws = new WebSocket.Server({ server });
 const clients = new Map();
-// const ws2port = 3000;
-// const ws2 = new WebSocket.Server({ port: ws2port });
-// const clients2 = new Map();
 
 // Send the page
 app.get('/', (req, res) => { });
@@ -48,46 +44,46 @@ ws.on('connection', (socket) => {
   // Generate user data
   let protocol = socket.protocol;
   let id = getNewID(clients);
-  let color = Math.floor(Math.random()*16777215).toString(16);
-  color = '0'.repeat(6 - color.length) + color;
+  let color = Math.floor(Math.random()*16777215).toString(16);  // Random number 0-16^6 (0-ffffff)
+  color = '0'.repeat(6 - color.length) + color;   // Pad 0's
 
   let connectionMessage = { type: "id", id: id, color: color };
   socket.send(JSON.stringify(connectionMessage));
   console.log("connection info sent!");
   console.log(JSON.stringify(connectionMessage));
 
-  let metadata = { protocol, id, color };
+  let userdata = { protocol, id, color };
 
-  clients.set(socket, metadata);
+  clients.set(socket, userdata);
 
   // Upon receiving a message, send it to all clients within the same protocol
   socket.on('message', (messageAsString) => {
     const message = JSON.parse(messageAsString);
-    const metadata = clients.get(socket);
+    const userdata = clients.get(socket);
 
     // Add identifying info
-    message.sender = metadata.id;
-    message.color = metadata.color;
+    message.sender = userdata.id;
+    message.color = userdata.color;
     const outbound = JSON.stringify(message);
     
     [...clients.entries()].forEach((client_data) => {
-      if (client_data[1].protocol == metadata.protocol) {
+      if (client_data[1].protocol == userdata.protocol) {
         client_data[0].send(outbound);
       }
     });
   });
 
   socket.on("close", () => {
-    const metadata = clients.get(socket);
+    const userdata = clients.get(socket);
 
     let message = { type: 'delete' };
     // Add identifying info
-    message.sender = metadata.id;
-    message.color = metadata.color;
+    message.sender = userdata.id;
+    message.color = userdata.color;
     const outbound = JSON.stringify(message);
 
     [...clients.entries()].forEach((client_data) => {
-      if (client_data[1].protocol == metadata.protocol) {
+      if (client_data[1].protocol == userdata.protocol) {
         client_data[0].send(outbound);
       }
     });
@@ -105,43 +101,9 @@ ws.on('connection', (socket) => {
 //   });
 // }
 function getNewID(clients) {
-  let max_id = [...clients.values()].reduce((acc, curr) => {
-    return curr.id > acc ? curr.id : acc;
-  }, 0);
+  // let max_id = [...clients.values()].reduce((acc, curr) => {
+  //   return curr.id > acc ? curr.id : acc;
+  // }, 0);
+  let max_id = Math.max(...clients.values());
   return max_id + 1;
 }
-console.log("wss up");
-
-
-// Start WebSocket for MultiPaint
-// ws2.on('connection', (socket) => {
-//   console.log('user connected');
-//   // console.log(socket);
-
-//   // Generate user data
-//   let id = uuidv4();
-//   const color = Math.floor(Math.random() * 360);
-//   const metadata = { id, color };
-
-//   clients2.set(socket, metadata);
-
-//   // Upon receiving a message, send it to all clients within the same protocol
-//   socket.on('message', (messageAsString) => {
-//     const message = JSON.parse(messageAsString);
-//     const metadata = clients2.get(socket);
-
-//     // Add identifying info
-//     message.sender = metadata.id;
-//     message.color = metadata.color;
-//     const outbound = JSON.stringify(message);
-
-//     [...clients2.keys()].forEach((client) => {
-//       client.send(outbound);
-//     });
-//   });
-
-//   socket.on("close", () => {
-//     console.log('user disconnected');
-//     clients2.delete(socket);
-//   });
-// });
