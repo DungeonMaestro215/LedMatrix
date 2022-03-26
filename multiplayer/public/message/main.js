846a532b1e895a9a14d519437a43b2085dd1bbb0
Denver Massey <denmas22@live.unc.edu>
2022-03-26 13:42:31 -0400

    fun animations

--- multiplayer/public/message/main.js
+++ multiplayer/public/message/main.js
@@ -3,6 +3,7 @@ window.onload = function () {
     const messageInput = document.getElementById("messageInput");
     const sendButton = document.getElementById("sendButton");
     const messageColor = document.getElementById("messageColor");
+    const sendDialog = document.getElementById("sendDialog");
 
     messageInput.addEventListener("keyup", (e) => {
         if (e.key === 'Enter') {
@@ -18,6 +19,17 @@ window.onload = function () {
         const color = messageColor.value;
         console.log(message);
         console.log(color);
+        
+        if (sendButton.classList.contains("flip")) {
+            sendButton.classList.add("flipback");
+            sendButton.classList.remove("flip");
+        } else {
+            sendButton.classList.add("flip");
+            sendButton.classList.remove("flipback");
+        }
+        
+        sendDialog.classList.remove("hidden");
+        setTimeout(() => sendDialog.classList.add("hidden"), 1500);
 
         axios({
             method: 'post',
