7d110285d7af3b57c4755a64a558fcd334706f6c
Denver Massey <denmas22@live.unc.edu>
2022-05-23 20:05:53 -0400

    working with matrix

--- MatrixPortal/individual_control_test.py
+++ MatrixPortal/individual_control_test.py
@@ -6,15 +6,30 @@ import board
 import displayio
 from adafruit_matrixportal.matrix import Matrix
 
-# config
-MATRIX_WIDTH = 64
-MATRIX_HEIGHT = 64
+# Config
+bit_depth = 1
+base_width = 64
+base_height = 32
+chain_across = 1
+tile_rows = 2
+serpentine = True
+
+MATRIX_WIDTH = base_width * chain_across
+MATRIX_HEIGHT = base_height * tile_rows
+
 palette = displayio.Palette(2)
 palette[0] = 0x000000   # Black
 palette[1] = 0xFFFFFF   # White
 
 # Setup objects
-matrix = Matrix(width=MATRIX_WIDTH, height=MATRIX_HEIGHT, bit_depth=3)
+matrix = Matrix(
+    width=MATRIX_WIDTH, 
+    height=MATRIX_HEIGHT, 
+    bit_depth=bit_depth,
+    serpentine=serpentine,
+    tile_rows=tile_rows
+)
+
 display = matrix.display
 print(dir(display))
 group = displayio.Group()
@@ -33,9 +48,11 @@ def set_pixel(bitmap, pixel, color):
     bitmap[row, col] = color
 
 while True:
-    for i in range(MATRIX_WIDTH*MATRIX_HEIGHT - 1):
+    for i in range(MATRIX_WIDTH*MATRIX_HEIGHT):
 
-        set_pixel(bitmap, i+1, 1)
-        set_pixel(bitmap, i, 0)
+        if (i < MATRIX_WIDTH*MATRIX_HEIGHT): set_pixel(bitmap, i, 1)
+        if (i > 0): set_pixel(bitmap, i-1, 0)
+        if (i == 0): set_pixel(bitmap, MATRIX_WIDTH*MATRIX_HEIGHT - 1, 0)
+        
         display.show(group)
         time.sleep(0.01)
